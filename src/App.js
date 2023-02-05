7bbe01f6a385402568729f9970bad1c2f7b95b85
Daniel Har-Oz <daniel.haroz2001@gmail.com>
2023-02-05 13:58:26 +0200

    Props Usage

--- src/App.js
+++ src/App.js
@@ -2,10 +2,8 @@ import Post from "./components/Post";
 
 function App() {
   return <main>
-    <Post/>
-    <Post/>
-    <Post/>
-    <Post/>
+    <Post author="Igor" body="Long live the Russian king."/>
+    <Post author="Freddy" body="1, 2, Freddy's coming for you..."/>
   </main>;
 }
 
--- src/components/Post.js
+++ src/components/Post.js
@@ -1,13 +1,9 @@
-const names = ["Swarley", "Torig"];
+function Post(props) {
 
-function Post() {
-
-    const chosenName = Math.random() > 0.5 ? names[0] : names[1];
     return (
         <div>
-            <p>{chosenName}</p>
-            <h1></h1>
-            <p>React is ok so far I guess.</p>
+            <p>{props.author}</p>
+            <p>{props.body}</p>
         </div>
     );
 }
