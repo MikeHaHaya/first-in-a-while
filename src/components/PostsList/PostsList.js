2d2f570a4865984a1b81a3c3583cd5f989caa6b5
Daniel Har-Oz <daniel.haroz2001@gmail.com>
2023-02-06 12:59:56 +0200

    PostsList Styling

--- src/components/PostsList/PostsList.js
+++ src/components/PostsList/PostsList.js
@@ -1,9 +1,9 @@
 import Post from "../Post/Post";
+import classes from "./PostsList.module.css";
 
 function PostsList() {
-
     return (
-       <ul>
+       <ul className={classes.posts}>
            <Post author="Igor" body="Long live the Russian king."/>
            <Post author="Freddy" body="1, 2, Freddy's coming for you..."/>
            <Post author="Darth Vader" body="I banged your mom Luke"/>
