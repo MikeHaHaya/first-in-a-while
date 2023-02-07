0777f2910295814e671d83f79d84304d68106fd0
Daniel Har-Oz <daniel.haroz2001@gmail.com>
2023-02-07 20:23:31 +0200

    Event Listeners

--- src/components/PostsList/PostsList.js
+++ src/components/PostsList/PostsList.js
@@ -1,14 +1,18 @@
 import Post from "../Post/Post";
 import classes from "./PostsList.module.css";
+import NewPost from "../NewPost/NewPost";
 
 function PostsList() {
     return (
-       <ul className={classes.posts}>
-           <Post author="Igor" body="Long live the Russian king."/>
-           <Post author="Freddy" body="1, 2, Freddy's coming for you..."/>
-           <Post author="Darth Vader" body="I banged your mom Luke"/>
-           <Post author="Luke" body="Noooooooo"/>
-       </ul>
+        <>
+            <NewPost/>
+            <ul className={classes.posts}>
+                <Post author="Igor" body="Long live the Russian king."/>
+                <Post author="Freddy" body="1, 2, Freddy's coming for you..."/>
+                <Post author="Darth Vader" body="I banged your mom Luke"/>
+                <Post author="Luke" body="Noooooooo"/>
+            </ul>
+        </>
     );
 }
 
