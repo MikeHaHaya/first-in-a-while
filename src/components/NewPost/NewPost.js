5c25643820002fd11815d233678496a0e35b1af8
Daniel Har-Oz <daniel.haroz2001@gmail.com>
2023-02-09 13:19:19 +0200

    Lifted State Up

--- src/components/NewPost/NewPost.js
+++ src/components/NewPost/NewPost.js
@@ -1,27 +1,15 @@
 import classes from './NewPost.module.css';
-import {useState} from "react";
-
-function NewPost() {
-    const [enteredBody, setEnteredBody] = useState("");
-    /** const stateData = useState("");
-     What stateData stores
-     stateData[0] -- current value
-     stateData[1] -- state updating function */
-
-    function changeBodyHandler(event) {
-        setEnteredBody(event.target.value);
-    }
+function NewPost(props) {
 
     return (
         <form className={classes.form}>
             <p>
                 <label htmlFor="body">Text</label>
-                <textarea id="body" required rows={3} onChange={changeBodyHandler}/>
+                <textarea id="body" required rows={3} onChange={props.onBodyChange}/>
             </p>
-            <p>{enteredBody}</p>
             <p>
                 <label htmlFor="name">Your name</label>
-                <input type="text" id="name" required/>
+                <input type="text" id="name" required onChange={props.onAuthorChange}/>
             </p>
         </form>
     );
--- src/components/PostsList/PostsList.js
+++ src/components/PostsList/PostsList.js
@@ -1,13 +1,24 @@
 import Post from "../Post/Post";
 import classes from "./PostsList.module.css";
 import NewPost from "../NewPost/NewPost";
+import {useState} from "react";
 
 function PostsList() {
+    const [enteredBody, setEnteredBody] = useState("");
+    const [enteredAuthor, setEnteredAuthor] = useState("")
+
+    function bodyChangeHandler(event) {
+        setEnteredBody(event.target.value);
+    }
+
+    function authorChangeHandler(event) {
+        setEnteredAuthor(event.target.value);
+    }
     return (
         <>
-            <NewPost/>
+            <NewPost onBodyChange={bodyChangeHandler} onAuthorChange={authorChangeHandler}/>
             <ul className={classes.posts}>
-                <Post author="Igor" body="Long live the Russian king."/>
+                <Post author={enteredAuthor} body={enteredBody}/>
                 <Post author="Freddy" body="1, 2, Freddy's coming for you..."/>
                 <Post author="Darth Vader" body="I banged your mom Luke"/>
                 <Post author="Luke" body="Noooooooo"/>
