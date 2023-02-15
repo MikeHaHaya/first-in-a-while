d24417d3c4584c72a4f6ed24d71adac2a0ee8f58
Daniel Har-Oz <daniel.haroz2001@gmail.com>
2023-02-15 17:56:30 +0200

    Handle Loading State

--- src/components/PostsList/PostsList.js
+++ src/components/PostsList/PostsList.js
@@ -7,18 +7,22 @@ import {useState, useEffect} from "react";
 function PostsList({isPosting, onStopPosting}) {
 
     const [posts, setPosts] = useState([]);
+    const [isFetching, setIsFetching] = useState(false);
     const [id, setId] = useState(0);
 
+    /** How to go get data without React being overloaded.
+     * this will make React load it just one time when the Component is launched. */
     useEffect(() => {
         async function fetchPosts() {
+            setIsFetching(true);
             const response = await fetch("http://localhost:8080/posts");
             const resData = await response.json();
             setPosts(resData.posts);
+            setIsFetching(false);
         }
 
         fetchPosts();
     }, [])
-
 
     function addPostHandler(postData) {
         fetch("http://localhost:8080/posts", {
@@ -33,9 +37,7 @@ function PostsList({isPosting, onStopPosting}) {
 
     // TODO -- Learn how to pass an ID properly
     function setIdHandler() {
-        setId(id + 1);
-        console.log(id);
-        return id;
+        setId(prevId => prevId + 1);
     }
 
     let modelContent;
@@ -50,21 +52,29 @@ function PostsList({isPosting, onStopPosting}) {
         );
     }
 
-    return (
-        <>
-            {modelContent}
-            <ul className={classes.posts}>
-                {posts.map(post => <Post key={setIdHandler} author={post.author} body={post.body}/>)}
-            </ul>
+    if (!isFetching) {
+        return (
+            <>
+                {modelContent}
+                <ul className={classes.posts}>
+                    {posts.map(post => <Post key={setIdHandler} author={post.author} body={post.body}/>)}
+                </ul>
 
-            {posts.length === 0 &&
-                <div style={{textAlign: 'center', color: 'white'}}>
-                    <h2>There are no posts yet.</h2>
-                    <p>Start adding some!</p>
-                </div>
-            }
-        </>
-    );
+                {posts.length === 0 &&
+                    <div style={{textAlign: 'center', color: 'white'}}>
+                        <h2>There are no posts yet.</h2>
+                        <p>Start adding some!</p>
+                    </div>
+                }
+            </>
+        );
+    } else {
+        return (
+            <div style={{textAlign: 'center', color: 'white'}}>
+            <p>Loading posts...</p>
+            </div>
+        )
+    }
 }
 
 export default PostsList;
